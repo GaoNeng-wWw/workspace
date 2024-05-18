bc7342d037fd44a09cd8ac1a965ed013cfc50ab9
GaoNeng-wWw <gaonenggg@outlook.com>
2024-05-18 21:25:09 +0800

    feat(analysis): exclude and include

--- src/commands/analysis.ts
+++ src/commands/analysis.ts
@@ -8,6 +8,11 @@ import {Project} from '@pnpm/types';
 import {Edge, Graph, Node, toDot, attribute as _} from 'ts-graphviz';
 import { toFile } from '@ts-graphviz/adapter';
 
+export type Option = {
+  excludes: string | undefined;
+  includes: string | undefined;
+}
+
 const draw = (g: Graph, nodes: Map<string, Node>, relation: [string, string[]][]) => {
   for (const [path, dependencies] of relation){
     if (!nodes.has(path)){
@@ -44,13 +49,34 @@ const sort = (graph: PackageGraph<Project>) => {
   const newPath = Array.from(map.entries()).sort((a,b) => b[1] - a[1]);
   const g: [string, string[]][] = [];
   for (const [path] of newPath){
-    const {dependencies} = graph[path];
+    const {dependencies} = graph[path] ?? {dependencies: []};
     g.push([path, dependencies]);
   }
   return g;
 };
 
-export const analysisDependencies = async (path: string = '.') => {
+const patternName = (patterns: RegExp[], name: string) => patterns.some(pattern => pattern.test(name));
+const createNode = (name: string, path:string, nodes: Map<string, Node>) => {
+  const node = new Node(name, {
+    [_.fontcolor]: '#ffffffcc',
+    [_.fillcolor]: '#222',
+    [_.shape]: 'box',
+    [_.style]: 'filled'
+  });
+  nodes.set(path, node);
+};
+
+const getPatterns = (str: string, sep: string = ',') => str.split(sep).filter((v) => v.length > 0).map((v) => v.split(',')).flat();
+
+export const analysisDependencies = async (
+  path: string = '.',
+  options:Option
+) => {
+  const {excludes='', includes=''} = options;
+  const excludeArray = getPatterns(excludes);
+  const includesArray = getPatterns(includes);
+  const includesRegExp = includesArray.map((include) => new RegExp(include));
+  const excludeRegExp = excludeArray.map((exclude) => new RegExp(exclude));
   const resolvePath = resolve(path);
   if (!existsSync(resolvePath)){
     error('pnpm-workspace.yaml not exists');
@@ -60,22 +86,26 @@ export const analysisDependencies = async (path: string = '.') => {
   const g = new Graph('', {
     [_.bgcolor]: '#141414',
     [_.rankdir]: 'LR',
-    // [_.splines]: false,
   });
   const nodes = new Map();
-  const graph = sort(allProjectsGraph);
+  const filtedGraph:PackageGraph<Project> = {};
   for (const [path, project] of Object.entries(allProjectsGraph)){
-    if (!project.package.manifest.name){
+    const {package: {manifest: {name}}} = project;
+    if (!name){
       continue;
     }
-    const node = new Node(project.package.manifest.name, {
-      [_.fontcolor]: '#ffffffcc',
-      [_.fillcolor]: '#222',
-      [_.shape]: 'box',
-      [_.style]: 'filled'
-    });
-    nodes.set(path, node);
+    if (excludeRegExp.length && patternName(excludeRegExp,name)){
+      info(`Ignore ${name}`);
+      continue;
+    }
+    if (includesRegExp.length && !patternName(includesRegExp, name)) {
+      continue;
+    }
+    info(`find ${name}`);
+    createNode(name, path, nodes);
+    filtedGraph[path] = project;
   }
+  const graph = sort(filtedGraph);
   draw(g, nodes, graph);
   g.attributes.graph.set(_.rank, 'same');
   const dot = toDot(g);
@@ -90,5 +120,8 @@ export const analysisDependencies = async (path: string = '.') => {
   info('Generate dependiencies success');
 };
 export default async (program: Command) => {
-  program.command('analysis [path]').action(analysisDependencies);
+  program.command('analysis [path]')
+  .option('-excludes --excludes [...name]', 'Exclude which packages')
+  .option('-includes --includes [...name]', 'Include which packages')
+  .action(analysisDependencies);
 };
